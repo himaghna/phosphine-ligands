c5c143bef7970236a7ed1594c54bf84f8c53f97b
himaghna <35175139+himaghna@users.noreply.github.com>
2020-09-13 13:16:04 -0400

    Add PCA support

--- data_exploration.py
+++ data_exploration.py
@@ -4,9 +4,11 @@ Description: Perform exploratory data analysis on the data-set
 """
 
 from argparse import ArgumentParser
+from os import mkdir
 import os.path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pickle
 from sklearn.decomposition import PCA
@@ -19,7 +21,7 @@ plt.rc('ytick',labelsize=16)
 
 def plot_pairplot(skip_frequency=True, save_location=None):
     """
-    Plot the satter plot for each pair of descriptors
+    Plot the scatter plot for each pair of descriptors
     Params ::
     skip_frequency: Boolean: True if frequency variables not considered.
         False otherwise
@@ -47,7 +49,7 @@ def plot_pairplot(skip_frequency=True, save_location=None):
 
 #plot_pairplot(save_location=r'D:\Research\Phosphine-Ligands\Figs\pair_plots')
 
-def do_PCA(save_location=None, variance_needed=0.90):
+def do_PCA(X, descriptor_names,save_location=None, variance_needed=0.90):
     """
     Do PCA on the data matrix X
     Params ::
@@ -106,12 +108,40 @@ def do_PCA(save_location=None, variance_needed=0.90):
         print(f'Saving {fname}')
         plt.savefig(os.path.join(save_location, fname))
     plt.close()
+    # loading plot
+    dat = pd.DataFrame(X, columns=descriptor_names)
+
+    def load_plot(score, coeff, labels=None):
+        xs = score[:, 0]
+        ys = score[:, 1]
+        n = coeff.shape[0]
+        scalex = 1.0 / (xs.max() - xs.min())
+        scaley = 1.0 / (ys.max() - ys.min())
+        #plt.scatter(xs * scalex, ys * scaley, c=y)
+        for i in range(n):
+            plt.arrow(0, 0, coeff[i, 0], coeff[i, 1], color='r', alpha=0.5)
+            if labels is None:
+                pass
+                # plt.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15,
+                #          "Var" + str(i + 1), color='g', ha='center',
+                #          va='center')
+            else:
+                plt.text(coeff[i, 0] * 1.15, coeff[i, 1] * 1.15, labels[i],
+                         color='g', ha='center', va='center')
+        plt.xlim(-0.5, 0.5)
+        plt.ylim(-0.5, 0.5)
+        plt.xlabel("PC{}".format(1), fontsize=24)
+        plt.ylabel("PC{}".format(2), fontsize=24)
+        plt.grid()
+
+    # Call the function. Use only the 2 PCs.
+    X_new = pca.transform(X_std)
+    load_plot(X_new[:, 0:2], np.transpose(pca.components_[0:2, :]))
+    plt.show()
     return n_eigenvectors_needed
 
-#n_eigenvectors = do_PCA(save_location=r'D:\Research\Phosphine-Ligands\Figs', \
- #   variance_needed=0.90)
 
-def check_correlation_with_target(descriptor_name):
+def plot_correlation_with_target(X, y, descriptor_names, out_dir=None):
     """
     Plot how a descriptor varies with the target
     Params ::
@@ -119,36 +149,73 @@ def check_correlation_with_target(descriptor_name):
     Returns ::
     None
     """
-    plt.scatter(X[:, descriptor_names.index(descriptor_name)], y, alpha=0.7)
-    plt.xlabel(descriptor_name, fontsize=20)
-    plt.ylabel('Target', fontsize=20)
-    plt.show()
+    for col in range(X.shape[1]):
+        plt.clf()
+        plt.scatter(X[:, col], y, alpha=0.4, s=100, c='red')
+        plt.xlabel(descriptor_names[col], fontsize=20)
+        plt.ylabel('Target', fontsize=20)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.tight_layout()
+        if out_dir is not None:
+            plt.savefig(os.path.join(out_dir,
+                                     f'{descriptor_names[col]}_response_parity.svg'))
+        else:
+            plt.show()
+
+
+def plot_input_distributions(X, descriptor_names, out_dir=None):
+    for col in range(X.shape[1]):
+        plt.clf()
+        plt.hist(X[:, col], color='green')
+        plt.ylabel('Frequency', fontsize=28)
+        plt.xlabel(descriptor_names[col], fontsize=28)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.tight_layout()
+        if out_dir is not None:
+            plt.savefig(os.path.join(out_dir,
+                                     f'{descriptor_names[col]}_distribution.svg'))
+        else:
+            plt.show()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-x', help='Path of X.p')
     parser.add_argument('-y', help='Path of y.p')
-    parser.add_argument('-dn', '--descriptor_names', 
+    parser.add_argument('-dn', '--descriptor_names',
                         help='Path of descriptor_names.p')
+    parser.add_argument('-od', '--output_dir', required=False, default=None,
+                        help='Optional output directory for saving images')
     args = parser.parse_args()
     
     X = pickle.load(open(args.x, "rb"))
     y = pickle.load(open(args.y, "rb"))
     descriptor_names = pickle.load(open(args.descriptor_names, "rb"))
-    
-    for col in range(X.shape[1]):
-        plt.hist(X[:, col], color='green')
-        plt.ylabel('Frequency', fontsize=28)
-        plt.xlabel(descriptor_names[col], fontsize=28)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        plt.show()
-        continue
-        plt.scatter(X[:, col], y, c='red', alpha=0.4, s=100)
-        plt.xlabel(descriptor_names[col], fontsize=20)
-        plt.ylabel('Response', fontsize=20)
-        plt.show()
+    output_dir = args.output_dir
+    n_eigenvectors = do_PCA(X=X,
+                            descriptor_names=descriptor_names,
+                            variance_needed=0.90)
+
+    # make directories
+    if output_dir is not None:
+        response_correlation_dir = os.path.join(output_dir,
+                                                      'Response_correlations')
+        mkdir(response_correlation_dir)
+        input_histogram_dir = os.path.join(output_dir,
+                                                 'Input_distributions')
+        mkdir(input_histogram_dir)
+    else:
+        response_correlation_dir = None
+        input_histogram_dir = None
+    plot_correlation_with_target(X,
+                                 y,
+                                 descriptor_names,
+                                 out_dir=response_correlation_dir)
+    plot_input_distributions(X,
+                             descriptor_names,
+                             out_dir=input_histogram_dir)
 
 
 
